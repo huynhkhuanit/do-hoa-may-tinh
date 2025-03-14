998fe7896ad4918085bfd4971c179903dae63e08
huynhkhuanit <huynhkhuanit@gmail.com>
2025-03-14 17:17:22 +0700

    Complete BT Do Hoa May Tinh Buoi 08

---
+++ thuc-hanh/buoi-08-plus/veHinVuong/veHinVuong.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include "graphics.h"
+#pragma comment(lib, "graphics.lib")
+using namespace std;
+
+struct Point {
+    int x, y;
+    Point() : x(0), y(0) {}
+    Point(int X, int Y) : x(X), y(Y) {}
+};
+
+class Square {
+private:
+    Point vertices[4];
+public:
+    Square(int x1, int y1, int size) {
+        vertices[0] = Point(x1, y1); // Ve dinh A
+        vertices[1] = Point(x1 + size, y1); // Ve dinh B
+        vertices[2] = Point(x1 + size, y1 + size); // Ve dinh C
+        vertices[3] = Point(x1, y1 + size); // Ve dinh D
+    }
+
+    // Ve canh tu form -> to (A to B)
+    void drawEdge(int from, int to, int color) {
+        setcolor(color);
+        line(vertices[from].x, vertices[from].y, vertices[to].x, vertices[to].y);
+    }
+
+    /*
+        vertices[0].x = vertices[3].x + dx = vertices[A].x  = vertices[D].x + dx
+        vertices[0].x = vertices[3].x + dx = vertices[A].x  = vertices[D].0 + 150
+        vertices[0].y = vertices[3].y + dy = vertices[A].150  = vertices[D].150 + 150
+    */
+    void translateEdge(int from, int to, int dx, int dy) {
+        vertices[to].x = vertices[from].x + dx;
+        vertices[to].y = vertices[from].y + dy;
+    }
+
+    bool isInWindow(int windowWidth, int windowHeight) {
+        for (int i = 0; i < 4; i++) {
+            if (vertices[i].x < 0 || vertices[i].x >= windowWidth ||
+                vertices[i].y < 0 || vertices[i].y >= windowHeight) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    int windowWidth = 640, windowHeight = 480;
+    initwindow(windowWidth, windowHeight, "Ve hinh vuong voi tinh tien");
+
+    int x1, y1, size;
+    cout << "Nhap toa do diem A (x1, y1): ";
+    cin >> x1 >> y1;
+    cout << "Nhap do dai canh hinh vuong: ";
+    cin >> size;
+
+    if (x1 < 0 || x1 >= windowWidth || y1 < 0 || y1 >= windowHeight) {
+        cout << "Toa do diem A nam ngoai cua so do hoa!\n";
+        closegraph();
+        return 1;
+    }
+
+    if (size <= 0) {
+        cout << "Do dai canh phai lon hon 0!\n";
+        closegraph();
+        return 1;
+    }
+
+    Square square(x1, y1, size);
+
+    if (!square.isInWindow(windowWidth, windowHeight)) {
+        cout << "Hinh vuong nam ngoai cua so do hoa!\n";
+        closegraph();
+        return 1;
+    }
+
+    // Ve A B
+    square.drawEdge(0, 1, YELLOW);
+    delay(1000);
+
+    // Ve A D
+    square.translateEdge(0, 3, 0, size);
+    square.drawEdge(0, 3, GREEN);
+    delay(1000);
+
+    // Ve B C
+    square.translateEdge(1, 2, 0, size);
+    square.drawEdge(1, 2, RED);
+    delay(1000);
+
+    // Ve C D
+    square.drawEdge(2, 3, BLUE);
+
+    getch();
+    closegraph();
+    return 0;
+}
---
+++ thuc-hanh/buoi-08/buoi-08/buoi-08.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include "graphics.h"
+#pragma comment(lib, "graphics.lib")
+using namespace std;
+
+struct Point {
+    int x, y;
+    Point() : x(0), y(0) {}
+    Point(int X, int Y) : x(X), y(Y) {}
+};
+
+void drawSquareAnimated(Point center, int size, int color) {
+    int halfSize = size / 2;
+    int left = center.x - halfSize;
+    int right = center.x + halfSize;
+    int top = center.y - halfSize;
+    int bottom = center.y + halfSize;
+
+    setcolor(color);
+
+    for (int i = 0; i <= halfSize; i += 2) {
+        line(left, top, left + i * 2, top);
+        delay(10);
+    }
+    for (int i = 0; i <= halfSize; i += 2) {
+        line(right, top, right, top + i * 2);
+        delay(10);
+    }
+    for (int i = 0; i <= halfSize; i += 2) {
+        line(right, bottom, right - i * 2, bottom);
+        delay(10);
+    }
+    for (int i = 0; i <= halfSize; i += 2) {
+        line(left, bottom, left, bottom - i * 2);
+        delay(10);
+    }
+}
+
+void scaleSquareAnimated(Point center, int initialSize, int scaleFactor) {
+    int finalSize = initialSize * scaleFactor;
+    int steps = 30;
+    float stepScale = (float)(scaleFactor - 1) / steps;
+
+    int halfSize = initialSize / 2;
+    Point topLeft(center.x - halfSize, center.y - halfSize);
+    Point topRight(center.x + halfSize, center.y - halfSize);
+    Point bottomRight(center.x + halfSize, center.y + halfSize);
+    Point bottomLeft(center.x - halfSize, center.y + halfSize);
+
+    float currentScale = 1.0;
+
+    for (int i = 0; i <= steps; i++) {
+        cleardevice();
+
+        int dx = (int)((center.x - topLeft.x) * currentScale);
+        int dy = (int)((center.y - topLeft.y) * currentScale);
+
+        Point newTopLeft(center.x - dx, center.y - dy);
+        Point newTopRight(center.x + dx, center.y - dy);
+        Point newBottomRight(center.x + dx, center.y + dy);
+        Point newBottomLeft(center.x - dx, center.y + dy);
+
+        setcolor(GREEN);
+        line(newTopLeft.x, newTopLeft.y, newTopRight.x, newTopRight.y);
+        line(newTopRight.x, newTopRight.y, newBottomRight.x, newBottomRight.y);
+        line(newBottomRight.x, newBottomRight.y, newBottomLeft.x, newBottomLeft.y);
+        line(newBottomLeft.x, newBottomLeft.y, newTopLeft.x, newTopLeft.y);
+
+        delay(50);
+        currentScale += stepScale;
+    }
+}
+
+int main() {
+    initwindow(640, 480, "Square Animation");
+
+    Point center(320, 240);
+    int initialSize;
+    int scaleFactor;
+
+    cout << "Nhap do dai canh ban dau cua hinh vuong: ";
+    cin >> initialSize;
+    cout << "Nhap he so scale (n): ";
+    cin >> scaleFactor;
+
+    drawSquareAnimated(center, initialSize, YELLOW);
+
+    delay(2000);
+
+    scaleSquareAnimated(center, initialSize, scaleFactor);
+
+    getch();
+    closegraph();
+
+    return 0;
+}
